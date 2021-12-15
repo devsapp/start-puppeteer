a997fc91b36ebff6e39cac991563cc1d4d257387
anycodes <service@52exe.cn>
2021-12-15 16:46:53 +0800

    update hook

--- puppeteer-container/hook/index.js
+++ puppeteer-container/hook/index.js
@@ -1,8 +1,4 @@
 async function preInit(inputObj) {
-
-}
-
-async function postInit(inputObj) {
     console.log(`\n     _______  __   __  _______  _______  _______  _______  _______  _______  ______   
     |       ||  | |  ||       ||       ||       ||       ||       ||       ||    _ |  
     |    _  ||  | |  ||    _  ||    _  ||    ___||_     _||    ___||    ___||   | ||  
@@ -11,13 +7,21 @@ async function postInit(inputObj) {
     |   |    |       ||   |    |   |    |   |___   |   |  |   |___ |   |___ |   |  | |
     |___|    |_______||___|    |___|    |_______|  |___|  |_______||_______||___|  |_|
                                         `)
+}
+
+async function postInit(inputObj) {
+
     console.log(`\n    Welcome to the puppeteer-app application
      This application requires to open these services: 
-         FC : https://fc.console.aliyun.com/
-         CR : https://cr.console.aliyun.com/
-     This application can help you quickly deploy the puppeteer-app project.
-         Full yaml configuration : https://github.com/devsapp/fc/blob/jiangyu-docs/docs/zh/yaml.md
-     The application homepage: https://github.com/devsapp/start-puppeteer\n`)
+         FC  : https://fc.console.aliyun.com/
+         ACR : https://cr.console.aliyun.com/
+     
+     * 额外说明：
+        1. 在s.yaml中配置了部分自动化流程，使用的时候可以根据自身需求进行删除和变更
+            - 部署之前，进行了项目的构建：s build --use-docker --dockerfile ./src/Dockerfile，要求您需要在当前环境安装好docker
+        2. 进入项目，对s.yaml进行更新，将customContainerConfig字段下的镜像地址替换自己ACR服务中成目标镜像地址；
+        3. 通过s deploy进行项目的部署；
+     \n`)
 }
 
 module.exports = {
--- puppeteer-nodejs/hook/index.js
+++ puppeteer-nodejs/hook/index.js
@@ -1,8 +1,4 @@
 async function preInit(inputObj) {
-
-}
-
-async function postInit(inputObj) {
     console.log(`\n     _______  __   __  _______  _______  _______  _______  _______  _______  ______   
     |       ||  | |  ||       ||       ||       ||       ||       ||       ||    _ |  
     |    _  ||  | |  ||    _  ||    _  ||    ___||_     _||    ___||    ___||   | ||  
@@ -11,12 +7,20 @@ async function postInit(inputObj) {
     |   |    |       ||   |    |   |    |   |___   |   |  |   |___ |   |___ |   |  | |
     |___|    |_______||___|    |___|    |_______|  |___|  |_______||_______||___|  |_|
                                         `)
+}
+
+async function postInit(inputObj) {
     console.log(`\n    Welcome to the puppeteer-app application
      This application requires to open these services: 
-         FC : https://fc.console.aliyun.com/
-     This application can help you quickly deploy the puppeteer-app project.
-         Full yaml configuration : https://github.com/devsapp/fc/blob/jiangyu-docs/docs/zh/yaml.md
-     The application homepage: https://github.com/devsapp/start-puppeteer\n`)
+         FC  : https://fc.console.aliyun.com/
+         NAS : https://nasnext.console.aliyun.com/
+     
+     * 额外说明：
+        1. 在s.yaml中配置了部分自动化流程，使用的时候可以根据自身需求进行删除和变更
+            - 部署之前，进行了项目的构建：s build --use-docker，要求您需要在当前环境安装好docker
+            - 部署之后，进行相关内容上传NAS，需要您提前开通NAS服务等
+        2. 进入项目，可以通过s deploy进行案例的的部署；
+     \n`)
 }
 
 module.exports = {
