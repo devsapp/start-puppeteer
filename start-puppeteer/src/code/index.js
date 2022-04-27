c790cf683b0c4367d8f1c5c37b521254f047ad41
rsonghuster <ls_huster@163.com>
2022-04-27 12:29:49 +0800

    update readme and auto scroll snapshot

--- start-puppeteer/src/code/index.js
+++ start-puppeteer/src/code/index.js
@@ -1,21 +1,43 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
+function autoScroll(page) {
+  return page.evaluate(() => {
+      return new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+              var scrollHeight = document.body.scrollHeight;
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+              if (totalHeight >= scrollHeight) {
+                  clearInterval(timer);
+                  resolve();
+              }
+          }, 100);
+      })
+  });
+}
+
 module.exports.handler = function (request, response, context) {
 
   (async () => {
     const browser = await puppeteer.launch({
       headless: true,
       args: [
-        '--no-sandbox',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
         '--disable-setuid-sandbox',
+        '--no-first-run',
+        '--no-zygote',
+        '--no-sandbox'
       ]
     });
 
     let url = request.queries['url'];
 
     if (!url) {
-      url = 'https://www.baidu.com';
+      url = 'https://www.serverless-devs.com';
     }
 
     if (!url.startsWith('https://') && !url.startsWith('http://')) {
@@ -28,21 +50,15 @@ module.exports.handler = function (request, response, context) {
     await page.goto(url, {
       'waitUntil': 'networkidle2'
     });
+    await page.setViewport({
+      width: 1200,
+      height: 800
+    });
+    await autoScroll(page)
 
     let path = '/tmp/example';
-    let contentType;
-
-    if (request.queries['pdf']) {
-      contentType = 'application/pdf';
-      await page.pdf({
-        path: path,
-        displayHeaderFooter: false
-      });
-    } else {
-      contentType = 'image/png';
-      await page.screenshot({ path: path, fullPage: true, type: 'png' });
-    }
-
+    let contentType = 'image/png';
+    await page.screenshot({ path: path, fullPage: true, type: 'png' });
     await browser.close();
 
     response.setStatusCode(200);
